1c097e00f686ac677ceaf46843e23274af21deb3
Paul Franco <mail@paulfran.co>
2020-11-02 19:42:46 -0800

    Initial commit

---
+++ app/src/main/java/co/paulfran/qrcodescanner/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package co.paulfran.qrcodescanner.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.paulfran.qrcodescanner.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
